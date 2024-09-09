350e7338fbea1a0d51890c2dac7b7149a0547b8b
Samkosken <sami.koskenranta@edu.keuda.fi>
2024-09-09 12:31:06 +0300

    js

---
+++ Javascript/p01.js
@@ -0,0 +1,29 @@
+// P01
+console.log("Hei maailma!")
+// p02
+var num1 = 5
+var num2 = 7
+summa = num1 + num2
+console.log(summa)
+// p03
+erotus = num1 - num2
+console.log(erotus);
+// p04
+tulo = num1 * num2
+console.log(tulo);
+// p05
+jako = num1 / num2
+console.log(jako);
+// p06
+var fahrenheit = num1 * 1.8 + 32;
+console.log(num1);
+console.log(fahrenheit);
+// p07
+var celcius = (num1 - 32) * 5 / 9;
+console.log(num1);
+console.log(celcius);
+// p08
+var kilometri = kilometri * maili;
+let kilometri = 100;
+var maili = 1.609344
+ 
