2c3e0e40950bea945f09ede902114ca7a984023d
Samkosken <sami.koskenranta@edu.keuda.fi>
2024-10-03 10:12:00 +0300

    lomaketarkistus

--- Formharj/lomaketarkistus.js
+++ Formharj/lomaketarkistus.js
@@ -1,30 +1,31 @@
 function tarkasta() {
 
     let enimi = document.getElementById("enimi").value;
-    let snimi = document.getElementById("snumi").value;
+    let snimi = document.getElementById("snimi").value;
     let losoite = document.getElementById("losoite").value;
     let posnumero = document.getElementById	("posnumero").value;
     let kaupunki = document.getElementById("kaupunki").value;
     let puhnumero = document.getElementById("puhnumero").value;
 
     if (enimi.trim() == "") {
-        alert("Anna etunimi!")
+        alert("Anna etunimi!");
 
     } else if (snimi.trim() == "") {
-        alert("Anna sukunimi!")
+        alert("Anna sukunimi!");
 
     } else if (losoite.trim() == "") {
-        alert("Anna lähiosoite!")
+        alert("Anna lähiosoite!");
 
     } else if (posnumero.trim() == "") {
-        alert("Anna postinumero!")
+        alert("Anna postinumero!");
+
     } else if (kaupunki.trim() == "") {
-        alert("Anna kaupunki!")
+        alert("Anna kaupunki!");
 
     } else if (puhnumero.trim() == "") {
-        alert("Anna puhelinnumero!")
+        alert("Anna puhelinnumero!");
     } else {
-        alert("Kiitos!")
+        alert("Kiitos!");
     }
     event.preventDefault();
 }
